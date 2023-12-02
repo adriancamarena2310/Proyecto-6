208c0567d2446d531d7e9b37f92454db62c7d897
Adrian Camarena <adriancamarena2310mail.com>
2023-12-02 14:27:06 -0600

    video-244 Insertar elementos al FormArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -27,15 +27,20 @@ export class DynamicPageComponent implements OnInit {
     ])
   });
 
+  public newFavorite: FormControl = new FormControl('', Validators.required );
+
   constructor( private fb: FormBuilder ) {}
 
-onSubmit():void {
-  if (this.myForm.invalid){
-    this.myForm.markAllAsTouched();
-    return;
-  }
-console.log(this.myForm.value)
-  this.myForm.reset();
+  onSubmit():void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray ) = this.fb.array([]);
+    this.myForm.reset();
 }
 
 isValidField( field: string ): boolean | null {
@@ -72,5 +77,18 @@ onDeleteFavorite( index:number ):void {
   this.favoriteGames.removeAt(index);
 }
 
+onAddToFavorites():void {
+
+  if ( this.newFavorite.invalid ) return;
+  const newGame = this.newFavorite.value;
+
+  // this.favoriteGames.push(  new FormControl( newGame, Validators.required ) );
+  this.favoriteGames.push(
+    this.fb.control( newGame, Validators.required )
+  );
+
+  this.newFavorite.reset();
+
+}
 
 }
