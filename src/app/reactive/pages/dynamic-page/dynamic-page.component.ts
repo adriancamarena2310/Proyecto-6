0694ed1dbb65ae4bf23150b72541cd092edf706e
Adrian Camarena <adriancamarena2310mail.com>
2023-12-02 14:08:20 -0600

    video-241 Formularios dinámicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html'
 })
 export class DynamicPageComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
   }
 
+  // public myForm2 = new FormGroup({
+  //   favoriteGames: new FormArray([])
+  // });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required ],
+      ['Death Stranding', Validators.required ],
+    ])
+  });
+
+  constructor( private fb: FormBuilder ) {}
+
+onSubmit():void {
+  if (this.myForm.invalid){
+    this.myForm.markAllAsTouched();
+    return;
+  }
+console.log(this.myForm.value)
+  this.myForm.reset();
+}
 }
