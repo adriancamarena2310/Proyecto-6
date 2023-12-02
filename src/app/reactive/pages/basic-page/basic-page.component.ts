896cc4d5cf8b3089fd6eb4557822c76f60404c0a
Adrian Camarena <adriancamarena2310mail.com>
2023-12-02 13:10:45 -0600

    video-236 Primeros pasos en formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html'
 })
 export class BasicPageComponent implements OnInit {
+ // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
 
-  constructor() { }
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  })
 
+constructor (private fb:FormBuilder){}
+
+onSave():void{
+  console.log(this.myForm.value);
+}
   ngOnInit() {
   }
+
 
 }
