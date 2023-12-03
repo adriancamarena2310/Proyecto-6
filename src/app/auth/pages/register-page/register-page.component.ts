bdb4cedd0af524612f38d6e566c99075cc467ad0
Adrian Camarena <adriancamarena2310mail.com>
2023-12-02 18:59:01 -0600

    video-255 Validadores asíncronos

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as customValidators from '../../../shared/validators/validators';
 import { ValidatorsService } from 'src/app/shared/service/validatos.service';
+import { EmailValidator } from 'src/app/shared/validators/email-validator.service';
 
 @Component({
   selector: 'app-register-page',
@@ -9,28 +10,30 @@ import { ValidatorsService } from 'src/app/shared/service/validatos.service';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor( private fb: FormBuilder,
-    private validatorsServiice: ValidatorsService
-    ) {}
-
   ngOnInit() {
   }
 
 
   public myForm: FormGroup = this.fb.group({
-    name:['',[Validators.required, Validators.pattern( this.validatorsServiice.firstNameAndLastnamePattern)]],
-    email:['',[Validators.required, Validators.pattern(this.validatorsServiice.emailPattern) ]],
-    username:['',[Validators.required, this.validatorsServiice.cantBeStrider]],
+    name:['',[Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email:['',[Validators.required, Validators.pattern(this.validatorsServiice.emailPattern) ],[this.emailVaidator]],
+
+  // email:['',[Validators.required, Validators.pattern(this.validatorsServiice.emailPattern) ],[new EmailValidator()]],
+    username:['',[Validators.required, customValidators.cantBeStrider]],
     password:['',[Validators.required, Validators.minLength(6)]],
     password2:['',[Validators.required]],
   });
 
     isValidField( field: string ) {
-      return this.validatorsServiice.isValidField( this.myForm, field );
+    //  TODO
     }
 
   onSubmit(){
     this.myForm.markAllAsTouched();
   }
 
+  constructor( private fb: FormBuilder,
+    private validatorsServiice: ValidatorsService,
+    private emailVaidator: EmailValidator
+    ) {}
 }
