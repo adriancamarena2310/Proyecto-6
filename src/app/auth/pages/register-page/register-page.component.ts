68c0ec41fda5fc99dc0382d03bd7aeac47098c32
Adrian Camarena <adriancamarena2310mail.com>
2023-12-02 18:17:13 -0600

    video-253 Validaciones de Email y Patrones

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from 'src/app/shared/validators/validators';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -15,10 +15,10 @@ export class RegisterPageComponent implements OnInit {
 
 
   public myForm: FormGroup = this.fb.group({
-    name:['',[Validators.required]],
-    email:['',[Validators.required]],
-    username:['',[Validators.required], cantBeStrider],
-    password:['',[Validators.required], Validators.minLength(6)],
+    name:['',[Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email:['',[Validators.required, Validators.pattern(customValidators.emailPattern) ]],
+    username:['',[Validators.required, customValidators.cantBeStrider]],
+    password:['',[Validators.required, Validators.minLength(6)]],
     password2:['',[Validators.required]],
   });
 
